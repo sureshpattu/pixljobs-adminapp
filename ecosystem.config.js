ecb8685a2e04d714e255b59a05ab702681d7ba67
Suresh Pattu <sureshpattu2002@gmail.com>
2019-07-08 14:46:12 +0530

    Pm2 name updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps:[
         {
-            name          :'Web',
+            name          :'Admin',
             script        :'./bin/www',
             watch         :false,
             env           :{
